be52bf3b309ab1dcc9b2635416e24d0bacc6e13d
saiy1000 <saiy1000@naver.com>
2021-10-03 15:24:51 +0900

    타입공부

---
+++ src/day_2/type1.java
@@ -0,0 +1,13 @@
+package day_2;
+
+public class type1 {
+	
+	public static void main(String[] args) {
+		int number=3;
+		String hanjae="26ªÏ¿Ã¥Ÿ";
+		
+		System.out.println(number);
+		System.out.println(hanjae);
+		
+	}
+}
